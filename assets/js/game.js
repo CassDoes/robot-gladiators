b281a5828a7ab36defcc13776a68a7188102e141
CassDoes <c.doexo@gmail.com>
2022-02-20 12:46:43 -0600

    fixed mixed text input bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,31 @@
+  var fightOrSkip = function() {
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    if (promptFight === "" || promptFight === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      return fightOrSkip();
+    }
+
+      promptFight = promptFight.toLowerCase();
+      
+      if (promptFight === "skip") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if (confirmSkip) {
+          window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+          playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+          return true;
+        }
+      }
+          return false;
+}
+
   var fight = function(enemy) {
 
   while (playerInfo.health > 0 && enemy.health > 0) {
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-      if (confirmSkip) {
-        window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
-      }
+    if (fightOrSkip()) {
+      break;
     }
-    // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
@@ -159,6 +171,7 @@ var getPlayerName = function() {
     console.log("Your robot's name is " + name);
     return name;
 };
+
 var playerInfo = {
   name: getPlayerName(),
   health: 100,
